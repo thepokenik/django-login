efa157b061d0b844aaf4056962f6b3fff1f28dbc
Nikolas.Melo <nikolas.melo@autaza.com>
2023-10-05 15:13:31 -0300

    Fix: Change email max length.

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=254, null=True)
     senha = models.CharField(max_length=12)
 
     def __str__(self):
