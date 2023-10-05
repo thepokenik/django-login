11fd9833284d0421422aaad010a1c79f69adb0fd
Nikolas.Melo <nikolas.melo@autaza.com>
2023-10-05 15:13:45 -0300

    Feat: Add login page route.

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, cadastro, editar, atualizar
+from .views import home, cadastro, editar, atualizar, login
 
 urlpatterns = [
     path('', home),
     path('cadastro/', cadastro, name="cadastro"),
     path('editar/<int:id>', editar, name="editar"),
     path('atualizar/<int:id>', atualizar, name="atualizar"),
+    path('login/', login, name='login')
 ]
